82e42384c160dfecf22985a8ecf87140a5952f2c
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-24 12:47:42 -0600

    add to food search spec

--- spec/features/food_search_spec.rb
+++ spec/features/food_search_spec.rb
@@ -5,30 +5,19 @@ RSpec.describe 'food search' do
 
     visit '/'
 
-    fill_in :q, with: 'sweet potatoes'
+    fill_in :q, with: 'sweet potato'
     click_on 'Search'
 
-    food = SearchFacade.new.find_foods('sweet potatoes')
+    food = SearchFacade.food_search('sweet potatoes')
 
     expect(current_path).to eq('/foods')
-    expect(page).to have_content("Total Found: 50")
-    expect(page).to have_content("Description: #{food.description}")
-    expect(page).to have_content("UPC Code: #{food.gtinUpc}")
-    expect(page).to have_content("Ingredients: #{food.ingredients}")
+    expect(page).to have_css('.food', count: 10)
+
+    within(first('.food')) do
+      expect(page).to have_css('.upc')
+      expect(page).to have_css('.description')
+      expect(page).to have_css('.brand')
+      expect(page).to have_css('.ingredients')
+    end
   end
-
 end
-
-# As a user,
-# When I visit "/"
-# And I fill in the search form with "sweet potatoes"
-# (Note: Use the existing search form)
-# And I click "Search"
-# Then I should be on page "/foods"
-# Then I should see a total of the number of items returned by the search.
-# Then I should see a list of ten foods that contain the ingredient "sweet potatoes"
-# And for each of the foods I should see:
-# - The food's GTIN/UPC code
-# - The food's description
-# - The food's Brand Owner
-# - The food's ingredients
